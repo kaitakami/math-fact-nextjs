e7d0c245a85c3c4901fdd98e14a0482baf4d9ccd
Kai Takami <kaitakami@protonmail.com>
2023-01-21 18:49:15 +0900

    Create function to consume deepl api

---
+++ app/services/japaneseTranslationAPI.ts
@@ -0,0 +1,28 @@
+const translateEnglish = require('deepl')
+interface Response {
+    data: { translations: [{ text: string }] }
+}
+
+const translate = (fact: string) => {
+    return translateEnglish({
+        free_api: true,
+        text: fact,
+        target_lang: "JA",
+        auth_key: process.env.PASSWORD
+    })
+        .then((data: Response) => (
+            {
+                translation: data.data.translations[0].text,
+                loading: false,
+                error: null
+            }))
+        .catch((err: Error) => (
+            {
+                translation: null,
+                loading: false,
+                error: err
+            }
+        ))
+}
+
+export default translate
